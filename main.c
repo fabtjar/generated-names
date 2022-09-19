dec83169fadb20942417e12c0236bc98e718edc7
Fab <fabianjarrett@gmail.com>
2022-09-19 19:56:50 +0100

    Debug with sames states as readme

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 #include "name.h"
 
 void debug_name(NAME n) {
-    printf("#%03d %s\n", n, NAME_names[n]);
+    printf("%s #%03d\n", NAME_names[n], n);
 }
 
 int main() {
-    debug_name(state_ape);
-    debug_name(state_bag);
-    debug_name(state_car);
-    debug_name(state_fox);
+    debug_name(state_ace);
+    debug_name(state_act);
+    debug_name(state_zap);
     debug_name(state_zip);
     return 0;
 };
